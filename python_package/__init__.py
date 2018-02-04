193ff38416de765f2636ce743fdd6c6e17deb46e
Zixuan Wang <wzixuanCC@outlook.com>
2018-02-04 14:04:57 -0500

    Added folder python_package, changed README

--- python_package/__init__.py
+++ python_package/__init__.py
@@ -1,4 +1,6 @@
-boundIn=__init__;
+boundIn="__init__";
 main=boundIn;
 if "__init__" == main:
     print("Initializing......");
+    boundOut=boundIn[0:4];
+    prefix_boundOut=main+boundIn+boundOut;
